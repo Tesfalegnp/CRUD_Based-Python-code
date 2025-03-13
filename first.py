7c7ceaaf4dc3ec20c43cf9edbbc9f0bc4d6ab5ee
Tesfalegn <peterhope935@gmail.com>
2025-03-13 10:00:25 +0300

    first_upload

---
+++ first.py
@@ -0,0 +1,71 @@
+                # Finctio Area 
+def search():
+        print("Searching")
+def delete():
+        print("Delete")
+def update():
+        print("Updating")
+def register():
+        print("Register Here")
+def display():
+        print("Printing...")
+        
+print("Hello  Every one Today i Started Pyhthon Code!\n\n")
+print("manu option\n 1-register\n 2-display\n 3-Search\n 4-Delete \n 5-Uptdate")
+choice=input("Enter your Choice")
+ne_ch=int(choice) #parsing
+if ne_ch==1:
+        register()
+elif 2 == choice:
+        display()
+        
+elif 3== ne_ch:
+        search()
+elif 4==ne_ch:
+        delete()
+elif 5==ne_ch:
+        update()
+    
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+            #The Sum of Two Number
+# def add(a,b):
+#     return a+b 
+# n1=int(input("Enter first Number!"))
+# n2=int(input("Enter secodse Number!"))
+# result=add(n1,n2)
+# print("You Entered: ",n1," And: ",n2," Then The Last Result Is:-",result)
+
+# For Loop
+# num=['ayyy','bs','ac']
+# num=[1j,2j,3e4]
+# # num='sume'
+# for i in num:
+#  n1=i
+# print(type(n1))
+
+        #Range
+# for i in range(-1,3):
+#     print("-",i)
+#     print("two")
+
+
+#         #if check form
+# x="how old are you?"
+# if "a" in x:
+#     print("Yes")
+# else:
+#     print("Not Are't")
+
+# txt=input("Write Something")
+# print("you wrote is:'","'\n\n\n")
+ 
